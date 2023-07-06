5f755b6b6f0d6e70cdf02f6736a393f2d6896fab
sriprakash24 <76991404+sriprakash24@users.noreply.github.com>
2023-07-06 20:07:33 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+// import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './Login';
+import Register from './Register';
+import { ToastContainer } from 'react-toastify';
+
+
+function App() {
+  return (
+    <div className="App">
+      <ToastContainer theme='colored' position='top-center'></ToastContainer>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login/>}></Route>
+        <Route path='/register' element={<Register/>}></Route>
+      </Routes>
+      </BrowserRouter>
+      
+    </div>
+  );
+}
+
+export default App;
